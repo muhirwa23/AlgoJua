872d8769d98d5f6baeab59d7045b9d274b029c4b
muhirwa23 <isaihalshakul09@gmail.com>
2026-01-24 14:15:45 +0200

    Auto-sync after agent response

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => (
             <Route path="/style-guide" element={<StyleGuide />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
-          <Route path="/admin" element={<Admin />} />
+          <Route path="/muhirwa" element={<Admin />} />
           <Route path="/aggregator-dashboard" element={<AggregatorDashboard />} />
           <Route path="/confirm/:token" element={<ConfirmSubscription />} />
           <Route path="/unsubscribe/:token" element={<Unsubscribe />} />
